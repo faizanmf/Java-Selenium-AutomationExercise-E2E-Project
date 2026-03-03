405e9f1acefce9b80f2578875abac0753b769b30
Mohammed Faizan <mohammed.1704056@srit.org>
2026-03-03 22:28:26 +0530

    Jenkins Test

--- src/main/java/com/utils/ScreenshotUtils.java
+++ src/main/java/com/utils/ScreenshotUtils.java
@@ -6,22 +6,40 @@ import org.openqa.selenium.TakesScreenshot;
 
 import java.io.File;
 import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class ScreenshotUtils {
 
     public static String captureScreenshot(String testName) {
+
         try {
             File src = ((TakesScreenshot) DriverManager.getDriver())
                     .getScreenshotAs(OutputType.FILE);
 
-            String path = ConfigReader.get("screenshot.path")
-                    + testName + ".png";
-            File dest = new File(path);
+            // Generate timestamp
+            String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss")
+                    .format(new Date());
+
+            // Get base screenshot path from config
+            String basePath = ConfigReader.get("screenshot.path");
+
+            // Create unique filename
+            String fileName = testName + "_" + timestamp + ".png";
+
+            File dest = new File(basePath + fileName);
 
             dest.getParentFile().mkdirs();
-            Files.copy(src.toPath(), dest.toPath());
 
-            return path;
+            Files.copy(
+                    src.toPath(),
+                    dest.toPath(),
+                    StandardCopyOption.REPLACE_EXISTING
+            );
+
+            return dest.getAbsolutePath();
+
         } catch (Exception e) {
             e.printStackTrace();
             return null;
